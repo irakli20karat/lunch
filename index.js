471220deaa203ec41227b7b8395512e5efed16b8
irakli20karat <thebroirakli@gmail.com>
2025-12-24 15:56:33 +0400

    msg

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const whosPaying = a => {
     return a[Math.floor(Math.random() * names.length)];
 }
 
-console.log(whosPaying(names));
+console.log(`${whosPaying(names)} is going to buy lunch today!`);
